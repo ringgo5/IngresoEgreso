7645ce9bb66b6e8afc1f722f4021605eb33a546f
ringgo5 <vicentem424@gmail.com>
2023-03-31 16:37:44 +0200

    añadido todos los componentes y html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+    {path: 'login', component:LoginComponent}, //ponemos la rutas aquí, Una para login,otra para register.
+    {path: 'register', component:RegisterComponent},
+    {
+      path: '',
+      component: DashboardComponent,
+      children: dashboardRoutes
+      
+    }, //esta para el dashboard,la necesitamos porque es la que carga una vez te registras/inicias sesion
+    {path: '**', redirectTo:''} //esto indica que cualquier otra ruta ira al dashboard tb
+
+];
+
+@NgModule({                                                     
+  imports: [RouterModule.forRoot(routes)],  //esto y lo de abajo ya venía configurado
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }  //debemos exportar este nombre a app.module.ts para indicarle que ya dispone de rutas
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//modulos:
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -24,10 +26,12 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     DetalleComponent,
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
   providers: [],
